3c6a33c68d159e1edd35b1165ec5601031adc5a9
Luís Henrique <lhmaruyama@gmail.com>
2023-01-19 18:34:35 -0300

    adicionar documento e gerar id automaticamente

--- crud.js
+++ crud.js
@@ -4,16 +4,22 @@ let add = document.querySelector("#add")
 add.onclick = await function(){
     let dateTime = document.querySelector(".datetime").value
     let notes = document.querySelector(".notes").value
-    //alert(dateTime + " " + notes)
-    //alert(fire.doc)
     if(dateTime == "" || notes == "" ){
-        //alert("Insira uma tarefa e uma data corretamente")
+        alert("Insira uma tarefa e uma data corretamente")
     }else{
         let dateNow = new Date().getTime()
         let dateTask = new Date(dateTime).getTime()
         if(dateTask >= dateNow){
             //alert(" A data informada é posterior a data atual")
-            fire.setDoc(fire.doc(db,"coleção","ID:000"),{dateTime,notes})
+
+            //fire.setDoc(fire.doc(db,"coleção","ID:000"),{dateTime,notes})
+
+            const newId = fire.doc(fire.collection(db,"teste coleção"))
+            fire.setDoc(newId,{dateTime,notes})
+
+            //const docId = fire.addDoc(fire.collection(db,"coleção"),{dateTime,notes})
+            //fire.doc(fire.collection(db,"teste"))
+
             let list = document.querySelector(".list")
             let li = document.createElement("li")
             li.textContent = notes + dateTime
@@ -21,7 +27,7 @@ add.onclick = await function(){
             let form = document.querySelector(".form")
             form.reset()
         }else{
-            //alert("A data informada é anterior a data atual")
+            alert("A data informada é anterior a data atual")
         }
     }
 }
